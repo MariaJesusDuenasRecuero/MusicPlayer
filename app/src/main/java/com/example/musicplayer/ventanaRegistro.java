e8a49ad2df8bc421fb67e0d5c68afc03b9a7dfa3
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-04-03 18:01:07 +0200

    Codigo Registro

--- app/src/main/java/com/example/musicplayer/ventanaRegistro.java
+++ app/src/main/java/com/example/musicplayer/ventanaRegistro.java
@@ -3,10 +3,16 @@ package com.example.musicplayer;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.musicplayer.Dominio.Usuario;
@@ -37,6 +43,7 @@ public class ventanaRegistro extends AppCompatActivity {
         txtCorreoElectronico = findViewById(R.id.txtCorreoElectronico);
         txtFechaNacimiento = findViewById(R.id.txtFechaNacimiento);
 
+
     }
 
     /**
@@ -47,20 +54,48 @@ public class ventanaRegistro extends AppCompatActivity {
      */
     public void oyente_btnRealizarRegistro(View view){
 
+        boolean comprobar_correo = comprobarCorreoElectronico();
 
         UsuarioDAO usuarioDAO = new UsuarioDAO();
         Usuario usuario = new Usuario("User3", "UsuarioPrueba3","321",
                 "615822963", "usuario2@gmail.com","02/11/1999");
 
-        //TODO comprobar_usuario_registrado_sistema, comprobar_correo
+        //TODO comprobar_usuario_registrado_sistema
         //El telefono no es obligatorio para registrase en el sistema
 
-        if(txtNombre.getText().equals("") || txtPassword.getText().equals("") ||
-                txtConfirmarPassword.getText().equals("") || txtFechaNacimiento.getText().equals("")){
+        if(txtNombre.getText().toString().equals("") || txtPassword.toString().equals("")
+                || txtConfirmarPassword.toString().equals("") || comprobar_correo == false){
 
+            //Comprobar si el nombre de usuario esta en uso
 
+            dialogoAviso("Registo Incompleto. Por favor rellene los campos que faltan.",ventanaRegistro.this);
+
+            comprobarDatosFormalarioRegistro(view);
 
         }
+        else{
+
+            if(txtPassword.getText().toString().equals(txtConfirmarPassword.getText().toString())){
+
+                txtNombre.setBackgroundColor(Color.rgb(0,255,0));
+                txtNombreUsuario.setBackgroundColor(Color.rgb(0,255,0));
+                txtPassword.setBackgroundColor(Color.rgb(0,255,0));
+                txtConfirmarPassword.setBackgroundColor(Color.rgb(0,255,0));
+                txtCorreoElectronico.setBackgroundColor(Color.rgb(0,255,0));
+
+            }
+            else{
+
+                txtPassword.setBackgroundColor(Color.rgb(255,0,0));
+                txtConfirmarPassword.setBackgroundColor(Color.rgb(255,0,0));
+
+                dialogoAviso("Las contraseñas no coinciden.",ventanaRegistro.this);
+
+                comprobarDatosFormalarioRegistro(view);
+
+            }
+        }
+
 
         /**
         int resInsert = usuarioDAO.insertarUsuario(ventanaRegistro.this, usuario);
@@ -74,27 +109,93 @@ public class ventanaRegistro extends AppCompatActivity {
             Toast.makeText(ventanaRegistro.this, "NO OK", Toast.LENGTH_SHORT).show();
         }
         */
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setMessage("Write your message here.");
-        builder1.setCancelable(true);
 
-        builder1.setPositiveButton(
-                "Yes",
+
+    }
+
+    private boolean comprobarCorreoElectronico(){
+
+        boolean correo_correcto = false;
+        String comprobar_correo = txtCorreoElectronico.getText().toString();
+
+        for (int i = 0; i<comprobar_correo.length(); i++){
+            if(comprobar_correo.charAt(i) == '@'){
+                correo_correcto = true;
+            }
+        }
+        return correo_correcto;
+    }
+
+    /**
+     *
+     * Descripcion: Metodo que comprueba los datos que faltan por completar
+     *
+     */
+    private void comprobarDatosFormalarioRegistro(View view){
+
+        //Datos Nombre
+
+        if(txtNombre.getText().toString().equals("")) {
+            txtNombre.setBackgroundColor(Color.rgb(200,0,0));
+        }
+        else{
+            txtNombre.setBackgroundColor(Color.rgb(0,255,0));
+        }
+
+        //Datos Contrasena
+
+        if(txtPassword.getText().toString().equals("")) {
+            txtPassword.setBackgroundColor(Color.rgb(200,0,0));
+        }
+        else{
+            txtPassword.setBackgroundColor(Color.rgb(0,255,0));
+        }
+
+        //Datos confirmar contrasena
+
+        if(txtConfirmarPassword.getText().toString().equals("")) {
+            txtConfirmarPassword.setBackgroundColor(Color.rgb(200,0,0));
+        }
+        else{
+            txtConfirmarPassword.setBackgroundColor(Color.rgb(0,255,0));
+        }
+
+        //Datos correo electronico
+
+        boolean correo_correcto_comprobacion = comprobarCorreoElectronico();
+
+        if(txtCorreoElectronico.getText().toString().equals("") || correo_correcto_comprobacion == false){
+            txtConfirmarPassword.setBackgroundColor(Color.rgb(200,0,0));
+        }
+        else{
+            txtConfirmarPassword.setBackgroundColor(Color.rgb(0,255,0));
+        }
+    }
+
+    /**
+     *
+     * Descripcion: Metodo que muestra un aviso al usuario dependiendo de las acciones que este realice
+     *
+     * @param aviso Mensaje personalizado dependiendo del mensaje del aviso
+     * @param context contexto en este caso es ventanaRegistro.this
+     */
+    private void dialogoAviso(String aviso, Context context){
+
+        AlertDialog.Builder dialogo_builder = new AlertDialog.Builder(context);
+        dialogo_builder .setMessage(aviso);
+        dialogo_builder.setCancelable(true);
+
+        dialogo_builder.setPositiveButton(
+                "OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
                     }
                 });
 
-        builder1.setNegativeButton(
-                "No",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
 
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
+        AlertDialog dialogo_alert = dialogo_builder.create();
+        dialogo_alert.show();
+
     }
 }
