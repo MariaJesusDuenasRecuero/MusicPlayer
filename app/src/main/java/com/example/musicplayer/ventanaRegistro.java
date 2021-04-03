dd8e7898ba29ea8da14a68a1f685ebd4f17d23ae
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-04-03 15:32:19 +0200

    Codigo Registro Arreglo boton login

--- app/src/main/java/com/example/musicplayer/ventanaRegistro.java
+++ app/src/main/java/com/example/musicplayer/ventanaRegistro.java
@@ -1,9 +1,12 @@
 package com.example.musicplayer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.musicplayer.Dominio.Usuario;
@@ -11,10 +14,29 @@ import com.example.musicplayer.Persistencia.UsuarioDAO;
 
 public class ventanaRegistro extends AppCompatActivity {
 
+    private EditText txtNombreUsuario;
+    private EditText txtNombre;
+    private EditText txtPassword;
+    private EditText txtConfirmarPassword;
+    private EditText txtTelefono;
+    private EditText txtCorreoElectronico;
+    private EditText txtFechaNacimiento;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_registro);
+
+        //Obtenemos las referencias a los elementos graficos de la GUI
+
+        txtNombreUsuario = findViewById(R.id.txtNombreUsuario);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtConfirmarPassword = findViewById(R.id.txtConfirmarPassword);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreoElectronico = findViewById(R.id.txtCorreoElectronico);
+        txtFechaNacimiento = findViewById(R.id.txtFechaNacimiento);
+
     }
 
     /**
@@ -30,6 +52,17 @@ public class ventanaRegistro extends AppCompatActivity {
         Usuario usuario = new Usuario("User3", "UsuarioPrueba3","321",
                 "615822963", "usuario2@gmail.com","02/11/1999");
 
+        //TODO comprobar_usuario_registrado_sistema, comprobar_correo
+        //El telefono no es obligatorio para registrase en el sistema
+
+        if(txtNombre.getText().equals("") || txtPassword.getText().equals("") ||
+                txtConfirmarPassword.getText().equals("") || txtFechaNacimiento.getText().equals("")){
+
+
+
+        }
+
+        /**
         int resInsert = usuarioDAO.insertarUsuario(ventanaRegistro.this, usuario);
 
         //int resInsert = usuarioDAO.borrarTabla(ventanaRegistro.this);
@@ -40,5 +73,28 @@ public class ventanaRegistro extends AppCompatActivity {
         else {
             Toast.makeText(ventanaRegistro.this, "NO OK", Toast.LENGTH_SHORT).show();
         }
+        */
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Write your message here.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
