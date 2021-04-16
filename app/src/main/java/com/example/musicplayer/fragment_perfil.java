39753a759ce01779108b547c8ac62d0f5b73289f
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-04-16 14:35:35 +0200

    Persistencia datos principales perfil terminada

--- app/src/main/java/com/example/musicplayer/fragment_perfil.java
+++ app/src/main/java/com/example/musicplayer/fragment_perfil.java
@@ -28,13 +28,15 @@ public class fragment_perfil extends Fragment {
 
     private TextView lblNombre_usuario_perfil_BBDD;
     private TextView lblNombre_perfil_BBDD;
+    private TextView lblTelefono_perfil_BBDD;
+    private TextView lblCorreoElectronico_perfil_BBDD;
+    private TextView lblFechaNacimiento_perfil_BBDD;
 
     public fragment_perfil(Usuario usuario) {
 
         this.usuario = usuario;
 
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,9 +47,15 @@ public class fragment_perfil extends Fragment {
 
         lblNombre_usuario_perfil_BBDD = (TextView) v.findViewById(R.id.lblNombreUsuario);
         lblNombre_perfil_BBDD = (TextView) v.findViewById(R.id.lblNombre_perfil_BBDD);
+        lblTelefono_perfil_BBDD = (TextView) v.findViewById(R.id.lblTelefono_perfil_BBDD);
+        lblCorreoElectronico_perfil_BBDD = (TextView) v.findViewById(R.id.lblCorreo_perfil_BBDD);
+        lblFechaNacimiento_perfil_BBDD = (TextView) v.findViewById(R.id.lblFechaNacimiento_perfil_BBDD);
 
         lblNombre_usuario_perfil_BBDD.setText(this.usuario.getNombreUsuario());
         lblNombre_perfil_BBDD.setText(" "+this.usuario.getNombre());
+        lblTelefono_perfil_BBDD.setText(" "+this.usuario.getTelefono());
+        lblCorreoElectronico_perfil_BBDD.setText(" "+this.usuario.getCorreo());
+        lblFechaNacimiento_perfil_BBDD.setText(" "+this.usuario.getFechaNacimiento());
 
         return v;
 
