91479d78707700ce3264120352ea480ac423fdb5
MJesusGit <MJesus.duenas1@alu.uclm.com>
2021-04-21 14:03:22 +0200

    Ventana reproduccion sin funcionalidad pero con metodos

--- app/src/main/java/com/example/musicplayer/activity_reproduccion.java
+++ app/src/main/java/com/example/musicplayer/activity_reproduccion.java
@@ -3,6 +3,7 @@ package com.example.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_reproduccion extends AppCompatActivity {
 
@@ -11,4 +12,21 @@ public class activity_reproduccion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproduccion);
     }
+
+    public void oyente_btnPlay(View view){
+
+    }
+    public void oyente_btnDetener(View view){
+
+    }
+    public void oyente_btnAnterior(View view){
+
+    }
+    public void oyente_btnSiguiente(View view){
+
+    }
+    public void oyente_btnAleatorio(View view){
+
+    }
+
 }
