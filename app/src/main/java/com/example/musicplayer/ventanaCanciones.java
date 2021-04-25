e9562c980a26e8748424ea62c57a4bc716777110
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-04-25 18:14:18 +0200

    Cancion avicii

--- app/src/main/java/com/example/musicplayer/ventanaCanciones.java
+++ app/src/main/java/com/example/musicplayer/ventanaCanciones.java
@@ -1,15 +1,35 @@
 package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.musicplayer.Adaptadores.AdaptadorListaCancion;
 import com.example.musicplayer.Dominio.Cancion;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class ventanaCanciones extends AppCompatActivity {
 
@@ -17,14 +37,22 @@ public class ventanaCanciones extends AppCompatActivity {
     private RecyclerView lstCanciones;
     private AdaptadorListaCancion adaptador_canciones;
 
+    private String [] items;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_canciones);
 
-        // Obtenemos una referencia a la lista grafica
-        lstCanciones = findViewById(R.id.lstArtistas);
+        //ActivityCompat.requestPermissions(ventanaCanciones.this, new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},111);
 
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.avicii);
+        mp.start();
+        // Obtenemos una referencia a la lista grafica
+        //lstCanciones = findViewById(R.id.lstArtistas);
+        listView = findViewById(R.id.listViewSong);
+        /**
         // Crear la lista de contactos y anadir algunos datos de prueba
         canciones = new ArrayList<Cancion>();
 
@@ -33,22 +61,80 @@ public class ventanaCanciones extends AppCompatActivity {
 
         adaptador_canciones = new AdaptadorListaCancion(canciones);
         lstCanciones.setAdapter(adaptador_canciones);
-
+            */
         // Metodo que rellena el array con datos de prueba
-        rellenarDatos();
+        runTimePermission();
+        //rellenarDatos();
     }
 
-    private void rellenarDatos(){
+    public void runTimePermission(){
 
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
-        canciones.add(new Cancion("a","45"));
+        Dexter.withContext(this).withPermission(READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        //rellenarDatos();
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+    }
+
+    public ArrayList<File> findSong (File file){
+
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        if(files != null){
+            Log.d("Debug_Excepcion","HO");
+            for(File singlefile: files){
+
+                if(singlefile.isDirectory() && !singlefile.isHidden()){
+
+                    arrayList.addAll(findSong(singlefile));
+
+                }
+                else{
+                    if(singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")){
+
+                        arrayList.add(singlefile);
+
+                    }
+                }
+
+            }
+        }
+        else{
+            Log.d("Debug_Excepcion","VACIO");
+        }
 
 
+        return arrayList;
+    }
+
+     void rellenarDatos(){
+
+        final ArrayList<File> mis_canciones = findSong(ventanaCanciones.this.getExternalFilesDir("/storage/emulated/0/Android/data"));
+
+        items = new String[mis_canciones.size()];
+
+        for(int i = 0; i<mis_canciones.size(); i++){
+
+            items[i] = mis_canciones.get(i).getName().toString().replace(".mp3", "").replace(".wav","");
+
+        }
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(ventanaCanciones.this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(myAdapter);
     }
 }
