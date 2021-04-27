68cb7046a9b72e35661c0b3fb045eeab3b581810
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-04-27 18:59:21 +0200

    Audio multimedia prueba comentado

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -142,13 +142,9 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void oyente_btnIniciarAplicacion(View view){
-        //Intent i = new Intent(this, activity_menu_principal.class );
-        //i.putExtra("nombre_usuario_registrado", txtNombreUsuarioLogin.getText().toString());
-        //startActivity(i);
 
-        byte [] audio = gestor_usuario_login.buscarAudio(MainActivity.this, txtNombreUsuarioLogin.getText().toString(), "ImagenPerfil");
-        playMp3(audio);
-
+        //byte [] audio = gestor_usuario_login.buscarAudio(MainActivity.this, txtNombreUsuarioLogin.getText().toString(), "ImagenPerfil");
+        //playMp3(audio);
 
         if(txtNombreUsuarioLogin.getText().toString().equals("") || txtPasswordLogin.getText().toString().equals("")){
 
